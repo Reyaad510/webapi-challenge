4b823c165779135dc3ff2883d4de3c667e9464e3
Reyaad Rafeek <reyaad510@gmail.com>
2019-05-18 13:09:11 -0400

    Completed get functionality for actions and projects

---
+++ data/actionRouter.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const Actions = require('./helpers/actionModel');
+const router = express.Router();
+
+
+
+
+// Read - Obtain actions
+
+router.get('/', async (req, res) => {
+    try {
+        const actions = await Actions.get();
+        res.status(200).json(actions);
+    } catch(err) {
+        res.status(500).json({ message: 'Error retrieving actions!' })
+    }
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,9 +2,20 @@ const express = require('express');
 
 const server = express();
 
+const actionRouter = require('./data/actionRouter');
+const projectRouter = require('./data/projectRouter');
+
 server.use(express.json());
 
 
+
+server.get('/', (req, res) => {
+    res.send(`<h1> Is this thing working? </h1>`)
+})
+
+// Router
+server.use('/api/actions', actionRouter);
+server.use('/api/projects', projectRouter);
 
 
 
